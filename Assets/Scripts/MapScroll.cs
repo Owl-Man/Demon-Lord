bc5285c3debcbbdbe2e2d6a927c7f2c44ac734fd
Fire Owl <61467254+Owl-Man@users.noreply.github.com>
2024-01-08 19:00:50 +0300

    Added map scroll

---
+++ Assets/Scripts/MapScroll.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class MapScroll : MonoBehaviour
+{
+    public float desensitization = 13;
+    
+    public float rightMaxPosition = 10f;
+    public float leftMaxPosition = 10f;
+    
+    public float topMaxPosition = 10f;
+    public float downMaxPosition = 10f;
+    
+    private Vector3 _initialMousePosition;
+    private Vector3 _initialObjectPosition;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _initialMousePosition = Input.mousePosition;
+            _initialObjectPosition = transform.position;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            float deltaX = (Input.mousePosition.x - _initialMousePosition.x) / desensitization;
+            float deltaY = (Input.mousePosition.y - _initialMousePosition.y) / desensitization;
+            
+            float newPositionX = _initialObjectPosition.x + deltaX;
+            float newPositionY = _initialObjectPosition.y + deltaY;
+            
+            newPositionX = Mathf.Clamp(newPositionX, rightMaxPosition * -1, leftMaxPosition);
+            newPositionY = Mathf.Clamp(newPositionY, topMaxPosition * -1, downMaxPosition);
+            
+            transform.position = new Vector3(newPositionX, newPositionY, _initialObjectPosition.z);
+        }
+    }
+}
