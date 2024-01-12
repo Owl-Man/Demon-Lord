3ae63e84b885ebba680f1cf2e002d339618ab72a
Fire Owl <61467254+Owl-Man@users.noreply.github.com>
2024-01-12 18:35:21 +0300

    Added sectors highlighting

---
+++ Assets/Scripts/ObjectHighlight.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class ObjectHighlight : MonoBehaviour
+{
+    private LineRenderer _lineRenderer;
+    private PolygonCollider2D _polygonCollider2D;
+
+    private Vector2[] _vertices;
+
+    private void Start()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+        _polygonCollider2D = GetComponent<PolygonCollider2D>();
+        
+        _vertices = _polygonCollider2D.points;
+
+        _lineRenderer.positionCount = _vertices.Length;
+    }
+
+    private void Update() => UpdateHighlight();
+
+    private void UpdateHighlight()
+    {
+        _vertices = _polygonCollider2D.points;
+        
+        for (int i = 0; i < _vertices.Length; i++)
+        {
+            Vector3 position = new Vector3(_vertices[i].x, _vertices[i].y, 1);
+            _lineRenderer.SetPosition(i, position);
+        }
+    }
+}
