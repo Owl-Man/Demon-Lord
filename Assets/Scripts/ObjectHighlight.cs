86d60eab0dea3325b90c02d1f7c8f9ab168296c4
Fire Owl <61467254+Owl-Man@users.noreply.github.com>
2024-01-17 19:09:13 +0300

    Update

--- Assets/Scripts/ObjectHighlight.cs
+++ Assets/Scripts/ObjectHighlight.cs
@@ -7,11 +7,16 @@ public class ObjectHighlight : MonoBehaviour
 
     private Vector2[] _vertices;
 
-    private void Start()
+    public bool IsEnabled { get; private set; }
+
+    private void Awake()
     {
         _lineRenderer = GetComponent<LineRenderer>();
         _polygonCollider2D = GetComponent<PolygonCollider2D>();
-        
+    }
+
+    private void Start()
+    {
         _vertices = _polygonCollider2D.points;
 
         _lineRenderer.positionCount = _vertices.Length;
@@ -21,6 +26,8 @@ public class ObjectHighlight : MonoBehaviour
 
     private void UpdateHighlight()
     {
+        if (!IsEnabled) return;
+        
         _vertices = _polygonCollider2D.points;
         
         for (int i = 0; i < _vertices.Length; i++)
@@ -28,5 +35,17 @@ public class ObjectHighlight : MonoBehaviour
             Vector3 position = new Vector3(_vertices[i].x, _vertices[i].y, 1);
             _lineRenderer.SetPosition(i, position);
         }
+    }
+
+    public void EnableHighlight()
+    {
+        _lineRenderer.enabled = true;
+        IsEnabled = true;
+    }
+
+    public void DisableHighlight()
+    {
+        _lineRenderer.enabled = false;
+        IsEnabled = false;
     }
 }
---
+++ Assets/Scripts/Sector.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class Sector : MonoBehaviour
+{
+    [SerializeField] private ushort armyCount;
+
+    private Camera _camera;
+    private ObjectHighlight _outline;
+
+    private void Start()
+    {
+        _camera = Camera.main;
+        _outline = GetComponent<ObjectHighlight>();
+        
+        _outline.DisableHighlight();
+    }
+
+    public void OnSelectSector()
+    {
+        if (_outline.IsEnabled)
+        {
+            _outline.DisableHighlight();
+        }
+        else
+        {
+            _outline.EnableHighlight();
+        }
+    }
+    
+    private void Update() 
+    {
+        if (Input.touchCount > 0) 
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPosition = _camera.ScreenToWorldPoint(touch.position);
+            RaycastHit2D hit = Physics2D.Raycast(touchPosition, Vector2.zero);
+
+            if (hit.collider.gameObject == gameObject)
+            {
+                OnSelectSector();
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Vector2 touchPosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+            
+            RaycastHit2D hit = Physics2D.Raycast(touchPosition, Vector2.zero);
+            
+            if (hit.collider.gameObject == gameObject)
+            {
+                OnSelectSector();
+            }
+        }
+    }
+}
